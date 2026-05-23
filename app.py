a41cfa86d11aa5bdfa322d3fb3f29eb85e2abcd8
yayoyhacipupu <kalyanakhow@gmail.com>
2026-05-23 10:50:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,3 +33,12 @@ if genre == ":rainbow[bu lina]":
 else:
     st.write("You didn't select comedy.")
 
+your-repository/
+├── pages/
+│   ├── page_1.py
+│   └── page_2.py
+└── your_app.py
+st.page_link("your_app.py", label="Home", icon="🏠")
+st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
+st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣", disabled=True)
+st.page_link("http://www.google.com", label="Google", icon="🌎")
